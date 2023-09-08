739937ac162c42475b1499f0945c528d118f6a86
yeancah-blingz <mustapha.yinka17@gmail.com>
2023-09-08 13:15:13 +0100

    index-3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customRed: '#ff0000',
+        customBlue: {light: '#00AEEF', dark: '#0055A0'}
+      }
+    },
   },
   plugins: [],
 }
