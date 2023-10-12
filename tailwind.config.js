dbb6e36a81a4c7a7edf1c5848d255f7e05ce25dc
yeancah-blingz <mustapha.yinka17@gmail.com>
2023-10-12 10:24:48 +0100

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     extend: {
       colors: {
         customRed: '#ff0000',
-        customBlue: {light: '#00AEEF', dark: '#0055A0'}
+        customBlue: {light: '#00AEEF',
+                    dark: '#0055A0',}
+      },
+      margin: {
+        '10': '3.75rem',
+        '20': '5rem',
       }
     },
   },
